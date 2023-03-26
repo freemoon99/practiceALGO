ce0abeda58b936052ba9bdb550bfe4ecc763da9f
GyuHyeon Park <soloist132@gmail.com>
2023-03-26 15:42:04 +0900

    [Gold V] Title: 용액, Time: 124 ms, Memory: 42172 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.py
+++ 백준/Gold/2467. 용액/용액.py
@@ -1,24 +1,22 @@
-value = 2000000000
 N = int(input())
-x, y = 0, 0
-l = 0
-r = N - 1
+li = list(map(int, input().split()))
+i, j = 0, 0
+left = 0
+right = N-1
 
-dragon_liquid = [int(x) for x in input().split()]
-# 문제에서 이미 오름차순으로 정렬
+ans = int(2e9)
 
-while l < r:
-    cur_value = dragon_liquid[l] + dragon_liquid[r]
-
-    if abs(cur_value) <= value:
-        x = dragon_liquid[l]
-        y = dragon_liquid[r]
-        value = abs(cur_value)
-
-    if cur_value <= 0:
-        l += 1
-
-    else: # cur_value > 0
-        r -= 1
-
-print(x, y)
+while left < right:
+  plus = li[left] + li[right]
+  
+  if abs(plus) <= ans:
+    i = li[left]
+    j = li[right]
+    ans = abs(plus)
+    
+  if plus <= 0:
+    left += 1  
+  else:
+    right -= 1
+    
+print(i, j)
