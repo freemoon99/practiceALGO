3763b39bdde2142b1b2edf523453d7412f8fe982
GyuHyeon Park <soloist132@gmail.com>
2023-04-06 21:29:45 +0900

    [Gold IV] Title: RGB거리 2, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+cost = [list(map(int, input().split())) for _ in range(N)]
+
+INF = int(1e9)
+ans = INF
+for i in range(3):
+    dp = [[INF, INF, INF] for _ in range(N)]
+    dp[0][i] = cost[0][i]
+    for j in range(1, N):
+        dp[j][0] = cost[j][0] + min(dp[j - 1][1], dp[j - 1][2])
+        dp[j][1] = cost[j][1] + min(dp[j - 1][0], dp[j - 1][2])
+        dp[j][2] = cost[j][2] + min(dp[j - 1][0], dp[j - 1][1])
+    for c in range(3):
+        if i != c:
+            ans = min(ans, dp[-1][c])
+print(ans)
