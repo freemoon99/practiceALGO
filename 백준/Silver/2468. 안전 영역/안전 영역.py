6d1c95f786ff4b1ab6e78ed8d6bce651d2a845a7
GyuHyeon Park <soloist132@gmail.com>
2023-09-24 00:33:51 +0900

    [Silver I] Title: 안전 영역, Time: 1508 ms, Memory: 32868 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,42 +1,35 @@
 import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(10 ** 6)
+sys.setrecursionlimit(10 ** 4)
 
 n = int(input())
-li = [list(map(int, input().split())) for _ in range(n)]
-maxNum = 0;
+loc = [list(map(int, input().split())) for _ in range(n)]
+locMin = min(map(min, loc))     # 지역에서 최소 높이
+locMax = max(map(max, loc))     # 지역에서 최대 높이
+dr = [0, 1, 0, -1]
+dc = [1, 0, -1, 0]
+ans = 1     # 아무것도 잠기 않았을 경우는 안전영역이 1이기 때문에
 
-for i in range(n):
-  for j in max(li):
-    if j > maxNum:
-      maxNum = j
+def dfs(row, col, h):
+    for i in range(4):  # 4방향 순회
+        nr = row + dr[i]
+        nc = col + dc[i]
+        if 0<=nr<n and 0<=nc<n and visited[nr][nc] == 0 and loc[nr][nc] > h:    # 다음 위치가 범위 안이고, 방문 가능하다면
+            visited[nr][nc] = 1
+            dfs(nr, nc, h)
 
-def dfs(x, y, h):
-  for i in range(4):
-    nx = x + dx[i]
-    ny = y + dy[i]
-    
-    if 0 <= nx < n and 0 <= ny < n and li[nx][ny] > h:
-      if v[nx][ny] == False:
-        v[nx][ny] = True
-        dfs(nx, ny, h)
 
-ans = 0
+for rain in range(locMin, locMax):
+    visited = [[0] * n for _ in range(n)]
+    cnt = 0
 
-dx = [0, 1, 0, -1]
-dy = [-1, 0, 1, 0]
+    for i in range(n):
+        for j in range(n):
+            # 현재 위치 (i, j)가 방문하지 않았고, 내리는 비의 양보다 크다면 체크
+            if visited[i][j] == 0 and loc[i][j] > rain:
+                cnt += 1
+                visited[i][j] = 1
+                dfs(i, j, rain)
 
-for i in range(maxNum):
-  v = [[False]*n for _ in range(n)]
-  cnt = 0
-  
-  for j in range(n):
-    for k in range(n):
-      if li[j][k] > i and v[j][k] == False:
-        cnt += 1
-        v[j][k] = True
-        dfs(j, k, i)
-
-  ans = max(ans, cnt)
+    ans = max(ans, cnt)
 
 print(ans)
