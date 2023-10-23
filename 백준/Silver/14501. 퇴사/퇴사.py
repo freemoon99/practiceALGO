3e66af9c05be4c71f2520900963f5b33408edd18
GyuHyeon Park <soloist132@gmail.com>
2023-10-23 21:27:08 +0900

    [Silver III] Title: 퇴사, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,14 +1,11 @@
-import sys
-input = sys.stdin.readline
-
 n = int(input())
-li = [list(map(int, input().split())) for _ in range(n)]
+schedule = [list(map(int, input().split())) for _ in range(n)]  # t,p 순으로 배열 저장 됨
 
-dp = [0 for _ in range(n+1)]
+dp = [0 for _ in range(n + 1)]
 
-for i in range(n):
-  for j in range(i+li[i][0], n+1):
-    if dp[j] < dp[i]+li[i][1]:
-      dp[j] = max(dp[j],  dp[i]+li[i][1])
-      
+for i in range(n):  # 7일 이므로 i는 날짜 기준
+    for j in range(i + schedule[i][0], n + 1):  # 현재일에 상담을 했다면 +기간 한 날 부터 시작
+        if dp[j] < dp[i] + schedule[i][1]:
+            dp[j] = max(dp[j], dp[i] + schedule[i][1])
+
 print(max(dp))
