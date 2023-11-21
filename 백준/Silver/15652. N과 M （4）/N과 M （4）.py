2d310264504582747525c1cc04bd76689241a996
GyuHyeon Park <soloist132@gmail.com>
2023-11-21 21:30:57 +0900

    [Silver III] Title: N과 M (4), Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,28 @@
+# 자연수 n,m이 주어졌을 때,
+# 1. 1~n까지 m개를 고른 수열(자연수)
+# 2. 중복 가능
+# 3. 비 내림차순(증가수열)
+
+
+n, m = map(int, input().split())
+
+selected = [0 for _ in range(m)]
+used = [0 for _ in range(n+1)]
+
+# 함수 정의 : 중복 가능한 수열 찾기
+def func(cnt):
+    if cnt == m:    # m개 를 골랐으면 출력
+        print(" ".join(map(str, selected)))
+    else: # 수 뽑기
+        # 시작할때, 처음 뽑는 수가 아니라면 이전에 뽑은 수보다 커야함
+        if cnt == 0:
+            start = 1
+        else:
+            start = selected[cnt-1]
+
+        for candidate in range(start, n+1):
+            selected[cnt] = candidate
+            func(cnt+1)
+            selected[cnt] = 0
+
+func(0)
