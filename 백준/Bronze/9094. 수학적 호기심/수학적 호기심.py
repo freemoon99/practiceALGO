d8aed561fd84f7e74cfe87fd229914d3f8e1e2f4
GyuHyeon Park <soloist132@gmail.com>
2023-11-27 22:42:08 +0900

    [Bronze III] Title: 수학적 호기심, Time: 4096 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/9094. 수학적 호기심/수학적 호기심.py
+++ 백준/Bronze/9094. 수학적 호기심/수학적 호기심.py
@@ -1,18 +1,14 @@
+import sys
+input = sys.stdin.readline
+
 t = int(input())
-dp = [[0]*101 for _ in range(101)]
-
-for i in range(1, 100):
-    for j in range(i + 1, 101):
-        dp[i][j] = (i**2)+(j**2)
-
 for _ in range(t):
     cnt = 0
     n, m = map(int, input().split())
 
-    for a in range(1, n-1):
+    for a in range(1, n+1):
         for b in range(a + 1, n):
-            if (dp[a][b] + m) % (a * b) == 0:
+            if ((a ** 2) + (b ** 2) + m) % (a * b) == 0:
                 cnt += 1
 
     print(cnt)
-
