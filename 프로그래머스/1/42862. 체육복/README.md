bb8b3e9f95eb38823e5c47c9ea3972e094863798
GyuHyeon Park <soloist132@gmail.com>
2023-12-14 00:23:17 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,6 +1,6 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 3일 23:54:55
+2023년 12월 4일 0:23:16
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,19 +1,20 @@
 def solution(n, lost, reserve):
-    # 잃어버렸는데 여분이 있는 학생들 제거
+    # 잃어버렸는데 여분이 있는 학생들 제거    
     dup = set(lost) & set(reserve)
     lost = list(set(lost) - dup)
     reserve = list(set(reserve) - dup)
     
-    answer = n
+    answer = n-len(lost)
     
     lost.sort()
+    
     for j in lost:
         if j-1 in reserve:
             reserve.remove(j-1)
+            answer += 1
         elif j+1 in reserve:
             reserve.remove(j+1)
-        else:
-            answer -= 1
+            answer += 1
 
     
     return answer
