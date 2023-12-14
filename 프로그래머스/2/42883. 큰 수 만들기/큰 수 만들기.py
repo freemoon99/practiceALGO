bbfc8e4b56930b57269e66701772bb2446d72cbe
GyuHyeon Park <soloist132@gmail.com>
2023-12-14 23:44:14 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,22 @@
+def solution(number, k):
+    answer = ''
+    temp = []
+    
+    for num in number:
+        while temp:
+            if temp[-1] < num and k > 0:
+                temp.pop()
+                k -= 1
+            else:
+                break
+            
+        temp.append(num)
+    
+    while len(temp) > len(number)-k:
+        print(1)
+        temp.pop()
+    
+    for i in temp:
+        answer += i
+    
+    return answer
