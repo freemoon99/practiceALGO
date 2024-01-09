6c3b8f9929fc23b26d03bf74d43960558745ed4b
GyuHyeon Park <soloist132@gmail.com>
2024-01-10 01:02:05 +0900

    [Gold III] Title: 피리 부는 사나이, Time: 564 ms, Memory: 80432 KB -BaekjoonHub

--- 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.java
+++ 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.java
@@ -47,24 +47,19 @@ public class Main {
     }
     
     private static void dfs(int x, int y) {
-    	visited[x][y] = area;
+    	visited[x][y] = 1;
     	
     	Pair direction = map.get(Character.toString(grid[x][y]));
     	
     	int nx = x + direction.x;
         int ny = y + direction.y;
         
-        if (0<=nx && nx<n && 0<=ny && ny<m) {
-            if (visited[nx][ny] == 0) {
-                dfs(nx, ny);
-            } else {
-                if (visited[nx][ny] == area) {
-                    cnt++;
-                }
-                area ++;
-            }
+        if (visited[nx][ny] == 0) {
+            dfs(nx, ny);
+        } else if (visited[nx][ny] == 1) {
+        	cnt++;
         }
-    	
+        visited[x][y] = 2;
     }
     
 	
