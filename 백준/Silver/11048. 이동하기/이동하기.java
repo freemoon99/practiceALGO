2e07e8f8ca19ddf9ef858192519b5591ebc09fc8
GyuHyeon Park <soloist132@gmail.com>
2024-01-11 23:08:43 +0900

    [Silver II] Title: 이동하기, Time: 544 ms, Memory: 73068 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n, m;
+	static int[][] miro;
+	
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        miro = new int[n+1][m+1];
+        
+        for (int i=1; i<n+1; i++) {
+        	st = new StringTokenizer(br.readLine());        	
+        	for (int j=1; j<m+1; j++) {
+        		miro[i][j] = Integer.parseInt(st.nextToken());
+        	}
+        }
+        
+        for (int i=1; i<n+1; i++) {    	
+        	for (int j=1; j<m+1; j++) {
+        		int max = Math.max(miro[i-1][j], Math.max(miro[i][j-1], miro[i-1][j-1]));
+        		
+        		miro[i][j] += max;
+        	}
+        }
+        
+        System.out.print(miro[n][m]);
+    }
+
+}
