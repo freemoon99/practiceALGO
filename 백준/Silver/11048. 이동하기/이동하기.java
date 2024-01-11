2a0adf61bc4c0a15f78192a9adbec64e23372b68
GyuHyeon Park <soloist132@gmail.com>
2024-01-11 23:15:47 +0900

    [Silver II] Title: 이동하기, Time: 520 ms, Memory: 72336 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.java
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -1,8 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
