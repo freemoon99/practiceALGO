ccd0adb21f473ac71eff34e779e936937bf12a1c
freemoon99 <soloist132@gmail.com>
2024-02-17 16:13:39 +0900

    정렬 사용 답안 추가

--- TopCoder/Cryptography.java
+++ TopCoder/Cryptography.java
@@ -1,3 +1,4 @@
+// 전체 탐색
 public class Cryptography {
   public long encrypt(int[] numbers) {
     long min = Long.MAX_VALUE;
@@ -12,4 +13,20 @@ public class Cryptography {
       
       return ans;
   }
+}
+
+// 정렬 사용
+import java.util.*;
+public class Cryptography {
+    public long encrypt(int[] numbers) {
+        long mul = 1;
+        Arrays.sort(numbers);
+        numbers[0]++;
+        
+        for (int n : numbers) {
+            mul *= n;
+        }
+        
+        return mul;
+    }
 }
